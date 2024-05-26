6105857bf4632bdc8955156b960f188b429b4b13
vlad-andrei-prodan <161964635+vlad-andrei-prodan@users.noreply.github.com>
2024-05-27 00:49:37 +0200

    Update main_script.js

--- main_script.js
+++ main_script.js
@@ -1,1 +1,7 @@
+window.addEventListener('scroll', function() {
+    var scrollTop = window.scrollY;
+    var opacity = 1 - (scrollTop / window.innerHeight); // Calculate opacity based on scroll position
 
+    // Apply opacity to video background
+    document.querySelector('.video-background').style.opacity = opacity < 0 ? 0 : opacity;
+});
